7016d2608f65cb966acca44b986b64e3becc4156
Mario <mario10843@outlook.com>
2022-12-31 10:59:48 -0300

    Modificação no layout
    Modificação nas cores do menu, footer adicionado, modificação no js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function menuShow(){
-    let menuMobile = document.querySelector('.mobile-menu')
+    let menuMobile = document.querySelector('.mobile')
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "./assets/img/open_menu.svg";
